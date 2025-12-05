ffa323be51f30dc9dbaa59efcc883f585866a27b
fredli4qooni <fredlifourqoni999@gmail.com>
2025-12-05 11:43:40 +0700

    add register feature

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,7 +21,7 @@ class RegisteredUserController extends Controller
     {
         return view('auth.register');
     }
-
+    
     /**
      * Handle an incoming registration request.
      *
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'customer',
         ]);
 
         event(new Registered($user));
